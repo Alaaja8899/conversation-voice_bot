a57df840da067a974dbbe11f52ec2a72274c0c51
Abdirizak abdullahi hussein <102373033+Abdirizak8899@users.noreply.github.com>
2023-03-27 12:33:46 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const micBtn = document.querySelector('.voice-btn')
 const speechRecognition = window.speechRecognition || window.webkitSpeechRecognition
-
+const FormData = document.querySelector('#ask')
 const recognition = new speechRecognition();
 recognition.onstart = (event) =>{
     micBtn.classList.add('active')
@@ -19,12 +19,23 @@ recognition.onresult = (event) =>{
     talkToThem(spokenWords)
 }
 
+FormData.addEventListener('submit', (event)=>{
+    event.preventDefault()
+    const spokenWords = FormData.querySelector('#text').value;
+    console.log(spokenWords)
+    document.querySelector('.message-body').innerHTML+= `<div class="user-msg"><p>${spokenWords}</p></div>`
+    $('.message-body').scrollTop($('.message-body')[0].scrollHeight);
+    talkToThem(spokenWords)
+    spokenWords.value = '';
+
+})
+
 
 function computerSpeech(words){
     const speech = new SpeechSynthesisUtterance();
-    speech.lang = 'eng-UK';
+    speech.lang = 'eng-US';
     speech.pitch = 0.9;
-    speech.volume = 1;
+    speech.volume = 2;
     speech.text = words;
     speech.rate =1;
 
@@ -35,7 +46,7 @@ function computerSpeech(words){
 function talkToThem(words){
     if (words.includes('hello') || words.includes('hi') || words.includes('hey')){
         let answer  = 'hi how i can help you '
-        document.querySelector('.message-body').innerHTML+= `<div class="bot-msg"><p>${answer}</p></div>`
+        document.querySelector('.message-body').innerHTML+= `<div class="bot-msg"><span class="bot-img"><img src="/support.png" alt="bot profile image"></span><p>${answer}</p></div>`
         $('.message-body').scrollTop($('.message-body')[0].scrollHeight);
     
         computerSpeech(answer)
@@ -44,16 +55,16 @@ function talkToThem(words){
         let answer  = 'my name is Zack'
         
         computerSpeech(answer)
-        computerSpeech('wha is your name')
+        computerSpeech('whats your name')
         
-        document.querySelector('.message-body').innerHTML+= `<div class="bot-msg"><p>${answer}</p></div>`
+        document.querySelector('.message-body').innerHTML+= `<div class="bot-msg"><span class="bot-img"><img src="/support.png" alt="bot profile image"></span><p>${answer}</p></div>`
         $('.message-body').scrollTop($('.message-body')[0].scrollHeight);
 
     }
     if (words.includes('are you a robot') || words.includes('your master') || words.includes('who created you')|| words.includes('who are you')|| words.includes('what are you')){
         let answer  = 'i am a robot founded by engineer Zack abdalla hussein'
 
-        document.querySelector('.message-body').innerHTML+= `<div class="bot-msg"><p>${answer}</p></div>`
+        document.querySelector('.message-body').innerHTML+= `<div class="bot-msg"><span class="bot-img"><img src="/support.png" alt="bot profile image"></span><p>${answer}</p></div>`
         $('.message-body').scrollTop($('.message-body')[0].scrollHeight);
 
         computerSpeech(answer)
@@ -62,7 +73,7 @@ function talkToThem(words){
     if (words.includes('Somalia') || words.includes('Where is Somalia ') || words.includes('Somalia located')){
         let answer  = 'Somalia, officially the Federal Republic of Somalia is a country in the Horn of Africa.'
 
-        document.querySelector('.message-body').innerHTML+= `<div class="bot-msg"><p>${answer}</p></div>`
+        document.querySelector('.message-body').innerHTML+= `<div class="bot-msg"><span class="bot-img"><img src="/support.png" alt="bot profile image"></span><p>${answer}</p></div>`
         $('.message-body').scrollTop($('.message-body')[0].scrollHeight);
 
         computerSpeech(answer)
@@ -71,26 +82,26 @@ function talkToThem(words){
     if (words.includes('hello google') || words.includes('hey google') || words.includes('Google')){
         let answer  = 'i am not google but we are freinds call me Zack please!'
 
-        document.querySelector('.message-body').innerHTML+= `<div class="bot-msg"><p>${answer}</p></div>`
+        document.querySelector('.message-body').innerHTML+= `<div class="bot-msg"><span class="bot-img"><img src="/support.png" alt="bot profile image"></span><p>${answer}</p></div>`
         $('.message-body').scrollTop($('.message-body')[0].scrollHeight);
 
         computerSpeech(answer)
 
     }
-    if (words.includes('are you good') || words.includes('how are you') || words.includes('how are you doing')|| words.includes('are you fine')|| words.includes('are you good')|| words.includes('how you doing')){
-        let answer  = 'i am doing greate alhamdulilah';
+    if (words.includes('are you good') || words.includes('how are you') || words.includes('how are you doing')|| words.includes('are you fine')|| words.includes('are you good')|| words.includes('how you doing') || words.includes('are you ok')){
+        let answer  = 'i am good';
 
-        document.querySelector('.message-body').innerHTML+= `<div class="bot-msg"><p>${answer}</p></div>`
+        document.querySelector('.message-body').innerHTML+= `<div class="bot-msg"><span class="bot-img"><img src="/support.png" alt="bot profile image"></span><p>${answer}</p></div>`
         $('.message-body').scrollTop($('.message-body')[0].scrollHeight);
 
         computerSpeech(answer)
         computerSpeech('what about you')
 
     }
-    if (words.includes('your developer') || words.includes('your engineer') || words.includes('do you know Zack')|| words.includes('Zack abdalla hussein')|| words.includes('Zack')|| words.includes('who is Zack') || words.includes('who developed you')){
+    if (words.includes('zack')  || words.includes('do you know Zack')|| words.includes('Zack abdalla hussein')|| words.includes('Zack')|| words.includes('who is Zack') || words.includes('who developed you')){
         let answer  = 'zack is 19 years old boy softwere engineer from somalia';
 
-        document.querySelector('.message-body').innerHTML+= `<div class="bot-msg"><p>${answer}</p></div>`
+        document.querySelector('.message-body').innerHTML+= `<div class="bot-msg"><span class="bot-img"><img src="/support.png" alt="bot profile image"></span><p>${answer}</p></div>`
         $('.message-body').scrollTop($('.message-body')[0].scrollHeight);
 
         computerSpeech(answer)
@@ -99,16 +110,16 @@ function talkToThem(words){
     if ( words.includes('somalia programmers')|| words.includes('somalia coding community')){
         let answer  = 'is gruop community on facebook they share codes tutorials extra';
 
-        document.querySelector('.message-body').innerHTML+= `<div class="bot-msg"><p>${answer}</p></div>`
+        document.querySelector('.message-body').innerHTML+= `<div class="bot-msg"><span class="bot-img"><img src="/support.png" alt="bot profile image"></span><p>${answer}</p></div>`
         $('.message-body').scrollTop($('.message-body')[0].scrollHeight);
 
         computerSpeech(answer)
 
     }
-    if ( words.includes('fuck you')|| words.includes('pussy')){
+    if ( words.includes('f*** you')|| words.includes('pussy')){
         let answer  = 'shame on you';
 
-        document.querySelector('.message-body').innerHTML+= `<div class="bot-msg"><p>${answer}</p></div>`
+        document.querySelector('.message-body').innerHTML+= `<div class="bot-msg"><span class="bot-img"><img src="/support.png" alt="bot profile image"></span><p>${answer}</p></div>`
         $('.message-body').scrollTop($('.message-body')[0].scrollHeight);
 
         computerSpeech(answer)
@@ -117,13 +128,115 @@ function talkToThem(words){
     if ( words.includes('where are you from')|| words.includes('your country') || words.includes('are you somalia') || words.includes('are you from somalia') ){
         let answer  = 'i am from somalia ';
 
-        document.querySelector('.message-body').innerHTML+= `<div class="bot-msg"><p>${answer}</p></div>`
+        document.querySelector('.message-body').innerHTML+= `<div class="bot-msg"><span class="bot-img"><img src="/support.png" alt="bot profile image"></span><p>${answer}</p></div>`
         $('.message-body').scrollTop($('.message-body')[0].scrollHeight);
 
         computerSpeech(answer)
 
     }
-   
+    if ( words.includes('i am all right')|| words.includes('i am doing greate') || words.includes('fine') || words.includes('good') ){
+        let answer  = 'glad to hear that';
+
+        document.querySelector('.message-body').innerHTML+= `<div class="bot-msg"><span class="bot-img"><img src="/support.png" alt="bot profile image"></span><p>${answer}</p></div>`
+        $('.message-body').scrollTop($('.message-body')[0].scrollHeight);
+
+        computerSpeech(answer)
+
+    }
+    if ( words.includes('love')|| words.includes('hate') || words.includes('angry') || words.includes('happy') ){
+        let answer  = 'i donn have feelings to do that you can help me to understand it more';
+
+        document.querySelector('.message-body').innerHTML+= `<div class="bot-msg"><span class="bot-img"><img src="/support.png" alt="bot profile image"></span><p>${answer}</p></div>`
+        $('.message-body').scrollTop($('.message-body')[0].scrollHeight);
+
+        computerSpeech(answer)
+
+    }
+    if ( words.includes('arrogant')|| words.includes('foolish') || words.includes('ass') || words.includes('fuck') ){
+        let answer  = 'shame on you you are not suppose to be here';
+
+        document.querySelector('.message-body').innerHTML+= `<div class="bot-msg"><span class="bot-img"><img src="/support.png" alt="bot profile image"></span><p>${answer}</p></div>`
+        $('.message-body').scrollTop($('.message-body')[0].scrollHeight);
+
+        computerSpeech(answer)
+
+    }
+    if ( words.includes('drink')|| words.includes('food') || words.includes('eating') || words.includes('hungry') || words.includes('water') ){
+        let answer  = 'shame on you you are not suppose to be here';
+
+        document.querySelector('.message-body').innerHTML+= `<div class="bot-msg"><span class="bot-img"><img src="/support.png" alt="bot profile image"></span><p>${answer}</p></div>`
+        $('.message-body').scrollTop($('.message-body')[0].scrollHeight);
+
+        computerSpeech(answer)
+
+    }
+    if ( words.includes('you are wrong')|| words.includes('wrong')  || words.includes('nope') || words.includes('no') ){
+        let answer  = 'sorry for that mistake i am still learning not good enough yet';
+
+        document.querySelector('.message-body').innerHTML+= `<div class="bot-msg"><span class="bot-img"><img src="/support.png" alt="bot profile image"></span><p>${answer}</p></div>`
+        $('.message-body').scrollTop($('.message-body')[0].scrollHeight);
+
+        computerSpeech(answer)
+
+    }
+    if ( words.includes('have good and night')|| words.includes('your good day') || words.includes('have nice day') || words.includes('have good day') || words.includes('day') ){
+        let answer  = 'have nice day too';
+
+        document.querySelector('.message-body').innerHTML+= `<div class="bot-msg"><span class="bot-img"><img src="/support.png" alt="bot profile image"></span><p>${answer}</p></div>`
+        $('.message-body').scrollTop($('.message-body')[0].scrollHeight);
+
+        computerSpeech(answer)
+    }
+    if ( words.includes('bye')|| words.includes('good bye ') || words.includes('leaving') || words.includes('going') || words.includes('jaw')   || words.includes('see you ' || words.includes('going') || words.includes('see you later'))){
+        let answer  = 'good bye , see you soon';
+
+        document.querySelector('.message-body').innerHTML+= `<div class="bot-msg"><span class="bot-img"><img src="/support.png" alt="bot profile image"></span><p>${answer}</p></div>`
+        $('.message-body').scrollTop($('.message-body')[0].scrollHeight);
+
+        computerSpeech(answer)
+    }
+    if ( words.includes('nice to meet you')|| words.includes('nice to see you') || words.includes('nice to talk to you') ){
+        let answer  = 'nice to meet you too';
+
+        document.querySelector('.message-body').innerHTML+= `<div class="bot-msg"><span class="bot-img"><img src="/support.png" alt="bot profile image"></span><p>${answer}</p></div>`
+        $('.message-body').scrollTop($('.message-body')[0].scrollHeight);
+
+        computerSpeech(answer)
+    }
+    if ( words.includes('my name is')|| words.includes('i am ') || words.includes('my name') ){
+        let answer  = 'nice to meet you ';
+
+        document.querySelector('.message-body').innerHTML+= `<div class="bot-msg"><span class="bot-img"><img src="/support.png" alt="bot profile image"></span><p>${answer}</p></div>`
+        $('.message-body').scrollTop($('.message-body')[0].scrollHeight);
+
+        computerSpeech(answer)
+    }
+    if ( words.includes('are you Muslim')|| words.includes('Muslim') || words.includes('muslim') || words.includes('your religion') || words.includes('Christian') || words.includes('christian') ){
+        let answer  = 'i am muslim alhamdulilah';
+
+        document.querySelector('.message-body').innerHTML+= `<div class="bot-msg"><span class="bot-img"><img src="/support.png" alt="bot profile image"></span><p>${answer}</p></div>`
+        $('.message-body').scrollTop($('.message-body')[0].scrollHeight);
+
+        computerSpeech(answer)
+    }
+    if ( words.includes('are you Muslim')|| words.includes('who is allah') || words.includes('created world') || words.includes('muslim god') || words.includes('god') || words.includes('allah') ){
+        let answer  = 'The name of the One, Supreme-Being also known as God in other faiths';
+
+        document.querySelector('.message-body').innerHTML+= `<div class="bot-msg"><span class="bot-img"><img src="/support.png" alt="bot profile image"></span><p>${answer}</p></div>`
+        $('.message-body').scrollTop($('.message-body')[0].scrollHeight);
+
+        computerSpeech(answer)
+    }
+    if (  words.includes('you helped me') || words.includes('thanks') || words.includes('thank you') ){
+        let answer  = 'i am here for your help';
+
+        document.querySelector('.message-body').innerHTML+= `<div class="bot-msg"><span class="bot-img"><img src="/support.png" alt="bot profile image"></span><p>${answer}</p></div>`
+        $('.message-body').scrollTop($('.message-body')[0].scrollHeight);
+
+        computerSpeech(answer)
+    }
+  
+    
     
 } 
 
