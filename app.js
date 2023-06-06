c649401e842ede63b2e5d7909baa5ee23c874747
Abdirizak abdullahi hussein <102373033+Abdirizak8899@users.noreply.github.com>
2023-06-06 10:15:48 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const micBtn = document.querySelector('.voice-btn')
 const speechRecognition = window.speechRecognition || window.webkitSpeechRecognition
+recognition.continuous = true;
+recognition.interimResults = true;
 const profile = document.querySelector('.profile')
 const recognition = new speechRecognition();
+
+
+
 const audio = new Audio('kulmis.mp3');
 const hor = new Audio('hordhac.mp3');
 
